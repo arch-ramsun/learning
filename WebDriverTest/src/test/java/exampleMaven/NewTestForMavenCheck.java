a982995494069359703e999efd59b03c98aae5a9
archana <arch.ramsun@gmail.com>
2018-11-17 13:51:51 +0530

    Modified commit

--- WebDriverTest/src/test/java/exampleMaven/NewTestForMavenCheck.java
+++ WebDriverTest/src/test/java/exampleMaven/NewTestForMavenCheck.java
@@ -8,26 +8,28 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class NewTestForMavenCheck {
-	
+
 	private WebDriver driver;
 	private String driverPath = "F:\\Archana\\geckodriver.exe";
-  @Test
-  public void basicTest() {
-	  driver.get("http://demo.guru99.com/test/guru99home/");  
-	  String title = driver.getTitle();				 
-	  Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  
-	  System.setProperty("webdriver.gecko.driver", driverPath);
-	  driver = new FirefoxDriver();
-	  
-  }
 
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();
-  }
+	@Test
+	public void basicTest() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+
+		System.setProperty("webdriver.gecko.driver", driverPath);
+		driver = new FirefoxDriver();
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 
 }
